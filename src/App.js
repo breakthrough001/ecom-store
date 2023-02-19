24fc3d95b3c9608447ba985b9ec0787943c6ed61
Joe Naples <jnaples@zorustech.com>
2023-02-19 07:45:06 -0500

    added nav links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { Routes, Route, Outlet } from "react-router-dom";
-import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the nav bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -19,9 +10,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
